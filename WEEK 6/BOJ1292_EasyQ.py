7e0c3cb88d1e3354cd736f75248423271f245b79
ShSeanLee <autotic77@gmail.com>
2022-04-18 19:32:41 +0900

    EasyQ

--- WEEK 6/BOJ1292_EasyQ.py
+++ WEEK 6/BOJ1292_EasyQ.py
@@ -1,9 +1,10 @@
 A, B = map(int, input().split())
 
-num_list = []
+num_list = []               # 1000번째 숫자까지 채워둠
 
-for i in range(1, 46):
-    for _ in range(i):
+# n이 n개씩 있으므로 1000번째 수는 45(1035번째 까지 45로 채워줌)
+for i in range(1, 46):      # 1 ~ 45까지 반복
+    for _ in range(i):      # n이 n개씩 있으므로
         num_list.append(i)
-# print(num_list[:10])
-print(sum(num_list[A-1:B]))
+        
+print(sum(num_list[A-1:B])) # 인덱스가 0부터 시작되므로 -1해줌
