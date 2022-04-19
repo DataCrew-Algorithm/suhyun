4b588f1be09cc7ef910344f01a4eaa5708cfa17e
ShSeanLee <autotic77@gmail.com>
2022-04-19 20:31:02 +0900

    Fibonacci

--- WEEK 6/BOJ2748_Fibonacci.py
+++ WEEK 6/BOJ2748_Fibonacci.py
@@ -1,7 +1,6 @@
 
 
-num_list = []
-
+num_list = [] # 피보나치 수열을 받을 리스트
 
 n0 = 0
 n1 = 1
@@ -10,13 +9,9 @@ n = int(input())
 num_list.append(n0)
 num_list.append(n1)
 
-if n == 0 or n == 1:
-    print(num_list[n])
 
-else:
-    for i in range(n-1):
-        sum_tmp = num_list[i] + num_list[i+1]
-        num_list.append(sum_tmp)
-        # print(num_list)
+for i in range(n-1):    # 이미 n1은 채워져 있으므로 n-1개만 채우면 됨
+    sum_tmp = num_list[i] + num_list[i+1] # 점화식
+    num_list.append(sum_tmp)
 
-    print(num_list[n])
+print(num_list[n])
