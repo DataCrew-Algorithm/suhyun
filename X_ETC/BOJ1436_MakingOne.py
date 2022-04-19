2e22e454dbf699b07461303e2d7bc0d082569360
ShSeanLee <autotic77@gmail.com>
2022-04-19 17:51:14 +0900

    BABBA

--- X_ETC/BOJ1436_MakingOne.py
+++ X_ETC/BOJ1436_MakingOne.py
@@ -1,19 +1,21 @@
 X = int(input())
 count = 0
 while X > 1:
-
     if X % 3 == 0:
-        X = X / 3
-        count += 1
+        while X % 3 == 0:
+            X = X / 3
+            count += 1
         if X == 1: break
 
     elif X % 2 == 0:
-        X = X / 2
-        count += 1
+        while X % 3 == 0:
+            X = X / 2
+            count += 1
         if X == 1: break
 
-    else: 
-        X -= 1
-        count += 1
+    else:
+        while (X % 3 != 0) or (X % 2 != 0):
+            X -= 1  
+            count += 1
         if X == 1: break
 print(count)
