1966fe803acd7b15e43a3ebf6f74a97dd210540c
ShSeanLee <autotic77@gmail.com>
2022-04-25 00:23:39 +0900

    GroupChecker

--- WEEK 6/BOJ1316_GroupChecker.py
+++ WEEK 6/BOJ1316_GroupChecker.py
@@ -2,14 +2,14 @@ N = int(input())
 
 def checker(str):
     counter = 0
-    alpha_list = [str[0]]
+    alpha_list = [str[0]]   # 첫 문자는 넣어둠
     
     for a in range(1, len(str)):
-        if str[a] != str[a-1]:
-            if str[a] in alpha_list:
-                counter -= 1
+        if str[a] != str[a-1]:          # 직전의 문자와 달랐는데
+            if str[a] in alpha_list:    # 리스트에 있으면
+                counter -= 1            # counter에 1을 빼주고 즉시 종료
                 break
-            else: 
+            else:                       # 리스트에 없는 경우에는 추가해줌.
                 alpha_list.append(str[a])
 
     return counter
@@ -19,4 +19,4 @@ ans = 0
 for _ in range(N):
     ans += checker(input())
 
-print(ans+N)
+print(ans+N)    # 전체 문자의 개수에서 Checker에서 체크된 만큼 빼줌
