cea7691ccae2bd406c70f13a339e6bed30866619
ShSeanLEE <autotic77@gmail.com>
2022-06-07 20:31:50 +0900

    PrintStars

--- WEEK13/BOJ10994_PrintStars.py
+++ WEEK13/BOJ10994_PrintStars.py
@@ -7,12 +7,6 @@ def stars(n):
     if n == 1:
         return ['*']
     else:
-        # star_list = []
-        # for i in range(4*(n-2)+1):
-        #     stars(n-1)[i] = '* ' + stars(n-1)[i] + ' *'
-        #     star_list.append('* ' + stars(n-1)[i] + ' *')
-        # print(star_list)
-
         add1 = '*' * (4 *(n-1)+1)               # 위 아랫줄에 추가 4(n-1) + 1
         add2 = '*' + ' '*(4 * (n-1)-1) + '*'
         return [add1, add2] + plus_stars(stars(n-1)) + [add2, add1]
@@ -21,7 +15,11 @@ N = int(input())
 print(*stars(N), sep = '\n')
     # add1 + '\n' + add2 + '\n'+ stars(n-1) + '\n' +add1 + '\n' + add2
 
-
+# star_list = []
+        # for i in range(4*(n-2)+1):
+        #     stars(n-1)[i] = '* ' + stars(n-1)[i] + ' *'
+        #     star_list.append('* ' + stars(n-1)[i] + ' *')
+        # print(star_list)
 
 
 
