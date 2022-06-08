e3d997c1d3a85a6ec19c5f36daadb900ab95c2ca
ShSeanLEE <autotic77@gmail.com>
2022-06-08 19:54:28 +0900

    MinHeap

---
+++ WEEK13/BOJ1927_MinHeap.py
@@ -0,0 +1,19 @@
+import heapq
+import sys
+
+nums = []
+N = int(input())
+for _ in range(N):
+    tmp = int(sys.stdin.readline().rstrip())
+    if nums:    # heap 안에 숫자가 있을때,
+        if tmp == 0:
+            print(heapq.heappop(nums))
+
+        else:
+            heapq.heappush(nums, tmp)
+    
+    else:       # heap이 비어있을 때,
+        if tmp == 0:
+            print(0)
+        else:
+            heapq.heappush(nums, tmp)
