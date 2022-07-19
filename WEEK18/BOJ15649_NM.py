8468468aaa5de0e52ec7d360eefbd52a9350cd19
ShSeanLEE <autotic77@gmail.com>
2022-07-19 20:18:21 +0900

    NM

--- WEEK18/BOJ15649_NM.py
+++ WEEK18/BOJ15649_NM.py
@@ -5,9 +5,10 @@ read = sys.stdin.readline
 
 N, M = map(int, read().split())
 
-perm = list(permutations([i for i in range(1, N+1)], M))
+perm = list(permutations([i for i in range(1, N+1)], M)) 
+# 순열 => 리스트안에 튜플로 받아줌
 
 for i in perm:
-    print(*i)
+    print(*i) # 튜플 빠져나오기 위한 언패킹
 
 
