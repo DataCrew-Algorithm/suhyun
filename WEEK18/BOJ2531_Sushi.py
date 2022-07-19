d574b8ce2a3ffeaa22462dd1ebf32a426e235b85
ShSeanLEE <autotic77@gmail.com>
2022-07-20 00:00:32 +0900

    Sushi

--- WEEK18/BOJ2531_Sushi.py
+++ WEEK18/BOJ2531_Sushi.py
@@ -1,4 +1,30 @@
-# ver1. deque를 이용한 풀이
+# ver1. deque를 이용한 풀이 -> 시간초과 (4%)
+import sys
+from collections import deque
+read = sys.stdin.readline
+
+N, d, k, c = map(int, read().split())
+
+
+nums = deque([]) # rotate를 위해서 deque를 씀
+for _ in range(N):
+    nums.append(int(read()))
+
+len_list = []   
+for _ in range(N):
+    tmp = deque([])                 
+    for i in range(k):              # 앞에서부터 k개의 초밥번호를 tmp에 담음
+        tmp.append(nums[i])
+    tmp.append(c)                   # 쿠폰에 나온 것을 담음
+    len_list.append(len(set(tmp))) # tmp를 set으로 바꾼 것에 몇개가 있는지를 추가
+    nums.rotate(1)
+
+print(max(len_list))
+
+
+# ver2
+
+
 import sys
 from collections import deque
 read = sys.stdin.readline
@@ -12,113 +38,86 @@ for _ in range(N):
 
 len_list = []
 for _ in range(N):
-    tmp = deque([])
+    tmp = set()
     for i in range(k):
-        tmp.append(nums[i])
-    tmp.append(c)
-    len_list.append(len(set(tmp)))
+        tmp.add(nums[i])
+    if c not in tmp:                  # not in으로 바꿔서 안들어 있을때만 +1해줌
+        len_list.append(len(tmp)+1)
+    else:
+        len_list.append(len(tmp))
     nums.rotate(1)
 
 print(max(len_list))
 
 
-# # ver2
-
-
-# import sys
-# from collections import deque
-# read = sys.stdin.readline
-
-# N, d, k, c = map(int, read().split())
-
-
-# nums = deque([])
-# for _ in range(N):
-#     nums.append(int(read()))
-
-# len_list = []
-# for _ in range(N):
-#     tmp = set()
-#     for i in range(k):
-#         tmp.add(nums[i])
-#     if c not in tmp:
-#         len_list.append(len(tmp)+1)
-#     else:
-#         len_list.append(len(tmp))
-#     nums.rotate(1)
-
-# print(max(len_list))
-
-
 # ver3
-# import sys
-# read = sys.stdin.readline
-
-# N, d, k, c = map(int, read().split())
-
-# nums = []
-# for _ in range(N):
-#     nums.append(int(read()))
-
-# left = 0
-# result = []
-# for _ in range(N):
-#     eaten = [0] * (d+1)
-
-#     for i in range(k):
-#         idx = left % N
-#         if eaten[nums[idx]] == 0:
-#             eaten[nums[idx]] = 1
-#         left += 1
-    
-#     cnt = 0
-#     for ss in eaten:
-#         if ss == 1:
-#             cnt += 1
-    
-#     if eaten[c] == 0:
-#         cnt += 1
-    
-#     result.append(cnt)
-
-# print(max(result))
-    
-    
-# ver4 블로그풀이 ?? 실패
 import sys
+read = sys.stdin.readline
 
-n, d, k, c = map(int, sys.stdin.readline().rsplit())
-arr = [int(sys.stdin.readline()) for _ in range(n)]
-lp, rp = 0, 0
-answer = 0
+N, d, k, c = map(int, read().split())
 
-while lp != n:
-    rp = lp + k
-    case = set()
-    addable = True
-    for i in range(lp, rp):
-        i %= n
-        case.add(arr[i])
-        if arr[i] == c: 
-            addable = False
+nums = []   # 리스트로 해봄
+for _ in range(N):
+    nums.append(int(read()))
 
-    cnt = len(case)
-    if addable: 
+left = 0
+result = []
+for _ in range(N):
+    eaten = [0] * (d+1) # 1 ~ d+1 의 스시 번호에 안먹었으면 0, 먹었으면 1
+
+    for i in range(k):
+        idx = left % N      # N으로 나눈 나머지를 idx로
+        if eaten[nums[idx]] == 0:
+            eaten[nums[idx]] = 1    # 0일때는 먹은것이니 1로 바꿔줌. 이미 먹은것인 1은 그대로 둠
+        left += 1
+    
+    cnt = 0
+    for ss in eaten:    # eaten 리스트에서 ss의 개수만 세서 cnt로 표시
+        if ss == 1:
+            cnt += 1
+    
+    if eaten[c] == 0:   # 쿠폰에서 나온 번호를 안먹었으면 +1
         cnt += 1
-    answer = max(answer, cnt)
-    lp += 1
+    
+    result.append(cnt)
 
-print(answer)
+print(max(result))
+    
+    
+# # ver4 블로그풀이 ?? 실패
+# import sys
+
+# n, d, k, c = map(int, sys.stdin.readline().rsplit())
+# arr = [int(sys.stdin.readline()) for _ in range(n)]
+# lp, rp = 0, 0
+# answer = 0
+
+# while lp != n:
+#     rp = lp + k
+#     case = set()
+#     addable = True
+#     for i in range(lp, rp):
+#         i %= n
+#         case.add(arr[i])
+#         if arr[i] == c: 
+#             addable = False
+
+#     cnt = len(case)
+#     if addable: 
+#         cnt += 1
+#     answer = max(answer, cnt)
+#     lp += 1
+
+# print(answer)
 
 
 # ver5 블로그풀이 defaultdict 이용한 투포인터
 import sys
 from collections import defaultdict
 
-# 접시 수, 초밥 가짓수, 연속해서 먹는 접시 수, 쿠폰 번호
 n, d, k, c = map(int, sys.stdin.readline().split())
 arr = [int(sys.stdin.readline()) for _ in range(n)]
-arr.extend(arr)   # 원형이라서 2개를 이어준다. 중요한 아이디어
+arr.extend(arr)   # 원형이라서 2개를 이어준다. !!중요한 아이디어
 left = 0
 right = 0
 dict_ = defaultdict(int)
