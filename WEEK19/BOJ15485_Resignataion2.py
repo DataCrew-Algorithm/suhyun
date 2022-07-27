00a656b012680694907fd8d36398e3992035e8b4
ShSeanLEE <autotic77@gmail.com>
2022-07-27 23:42:54 +0900

    Resignation2

--- WEEK19/BOJ15485_Resignataion2.py
+++ WEEK19/BOJ15485_Resignataion2.py
@@ -19,7 +19,7 @@
 
 # for i in range(N):
 #     idx = i
-#     sum_rev = revenues[i]
+#     sum_rev = revenues[i]   # 누적수익
 
 #     while (idx + days[idx]< N) & (idx < N):
 #         idx += days[idx]
@@ -54,16 +54,34 @@ for i in range(n):
     x,y = map(int,read().split())
     days.append(x)
     revenues.append(y)
+
 M = 0                       # 누적 수익 M
 for i in range(n):          # i에서 출발하는 최대 수익을 가진 루트로 계속 갱신
-    M = max(M, dp[i])  
+    M = max(M, dp[i])       # M을 교체하는 과정
     if i + days[i] > n :    # index가 넘어설 때는 for문 위로 돌아감
         continue
-    dp[i+days[i]] = max(M + revenues[i], dp[i+days[i]]) 
-    # 이동할 다음 루트의 원래 값과 새로운 수익을 더한값중 큰 값으로 대체
-# print(dp)
+    dp[i+days[i]] = max(dp[i+days[i]], M + revenues[i]) # dp를 바꾸는 과정
+    # 이동할 다음 루트의 원래 값과/ 새로운 수익을 더한값중 큰 값으로 대체
+print(dp)
 print(max(dp))
 
 
+'''
+idx
+0 -> 3 -> 4
 
+revenues
+10   20    15
+
+days
+3      1    2   
+
+0 / M = 0, dp[3] = max(0+10, dp[3]=0) = 10
+1 / M = 0, dp[6] = max(0+20, dp[6]=0) = 20
+2 / M = 0, dp[3] = max(0+10, dp[3]=10) = 10
+3 / M = 10, dp[4] = max(10+20, d[4]=0) = 30
+4 / M = 30, dp[6] = max(30+15, d[6]=0) = 45
+5 / M = 30, dp[7] x
+6 / M = 45, dp[200] x 
+'''
 
