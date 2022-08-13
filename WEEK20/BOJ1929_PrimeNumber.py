5ae80d09321a95638d936721b24f3d1a92d04e01
ShSeanLEE <autotic77@gmail.com>
2022-08-13 17:05:38 +0900

    PrimeNumber

--- WEEK20/BOJ1929_PrimeNumber.py
+++ WEEK20/BOJ1929_PrimeNumber.py
@@ -1,20 +1,19 @@
-# # 시간초과
-# import sys
+# 시간초과
+import sys
 
-# input = sys.stdin.readline
+input = sys.stdin.readline
 
-# M, N = map(int, input().split())
+M, N = map(int, input().split())
 
-# for i in range(M, N+1):
-#     for j in range(2, i):
-#         if i % j == 0:      # 나누어 떨어진다. = 소수가 아니다.
-#             break
-#     else:
-#         print(i)
+for i in range(M, N+1):
+    for j in range(2, i):   # i보다 작은수인 j로 나누어 떨어지면 소수가 아니다
+        if i % j == 0:      # 나누어 떨어진다. = 소수가 아니다.
+            break           # 그 즉시 해당 숫자는 소수가 아니므로 탈락
+    else:
+        print(i)
 
 # 블로그 힌트 -> i의 제곱근까지만 하면 됨
-
-# 시간초과
+# 시간 너무 오래걸림 5708ms -> 에라스토스의 체로 구하는게 빠름
 import sys
 
 input = sys.stdin.readline
@@ -30,3 +29,24 @@ for i in range(M, N+1):
                 break
         else:
             print(i)
+
+# 에라스토스의 체를 이용한 풀이
+# 2부터 소수에 어떤 수를 곱한것은 소수가 아니다라는 성질을 이용하여 차례대로 False로 바꿔가며 제거해 나아가는 방식
+def erastosSieve(end): # 시작, 끝 범위 수 입력 (0 ~ n까지)
+    erastos = [True] * (end + 1) # 소수 판별 여부를 담는 리스트
+    erastos[0], erastos[1] = False, False # 0과 1은 소수가 아니므로 False로 초기화
+    
+    for i in range(2, int(end ** 0.5) + 1): # 시간 단축하기 위해 제곱근 사용
+        if erastos[i] == True: # i가 소수인 경우
+            j = 2
+            while i * j <= end:
+                erastos[i * j] = False # i를 제외한 i의 모든 배수 지우기
+                j += 1
+                
+    return erastos
+
+start, end = map(int, input().split())
+
+for i, v in enumerate(erastosSieve(end)):
+    if i>= start and v:
+        print(i)
